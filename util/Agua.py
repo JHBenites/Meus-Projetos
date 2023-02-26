d8d3a28a8ac83f84375a37fd4acd2e377f3a95c3
JHBenites <jhonatanarsbenites@gmail.com>
2023-02-26 15:00:43 -0400

    água

---
+++ util/Agua.py
@@ -0,0 +1,6 @@
+nome = input("Olá, qual é o seu nome:")
+peso = input(f'{nome}, quanto você pesa:')
+min = input(f'{nome}, durante quantas horas por dia tem disponibilidade /'
+            f'para beber água:')
+copo = 180
+min *= 160
