66e433cdfe7628367cab97dec9d26f1811e37d2e
JHBenites <jhonatanarsbenites@gmail.com>
2023-02-26 17:55:55 -0400

    agua

--- util/Agua.py
+++ util/Agua.py
@@ -1,6 +1,23 @@
 nome = input("Olá, qual é o seu nome:")
-peso = input(f'{nome}, quanto você pesa:')
-min = input(f'{nome}, durante quantas horas por dia tem disponibilidade /'
-            f'para beber água:')
+peso =float(input(f'{nome}, quanto você pesa:'))
+min = int(input(f'{nome}, durante quantas horas por dia você tem disponibilidade '
+            f'para beber água:'))
 copo = 180
-min *= 160
+min *= 60
+ml = 35
+
+agua_ideal = peso * ml
+copos = round(agua_ideal / copo)
+tempo = round(min/copos)
+
+print(f'\n{nome}, você deve tomar {copos} copos durante o dia a cada {tempo} minutos. Boa Sorte!') #printar quantidade de copos
+
+
+
+
+
+
+
+
+
+
